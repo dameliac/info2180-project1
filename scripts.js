3008905c961853f0aa0614e58b4f83a4c3806c24
Damelia Coleman <dameliac@outlook.com>
2023-11-05 11:57:45 -0500

    Added functionality to the subscribe button

--- scripts.js
+++ scripts.js
@@ -1,22 +1,31 @@
 /* Add your JavaScript to this file */
 window.onload = function newsletter(){
-
-    let email = document.getElementById("email").value;
-    let submit = document.getElementsByTagName("button")[0];
-    let displaymsg = document.getElementsByClassName("message");
+    let form = document.forms[0];
+    form.placeholder = "Enter your email address";
+    let submit = document.querySelector(".newsletter").querySelector(".btn");
+    let displaymsg = document.querySelector(".message");
+    let emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 
 
     submit.addEventListener('click', function(){
-        console.log(email);
-        if (email == ""){
-            displaymsg.innerHTML = 'Please enter a valid email address.';
+        let email = form.elements["email"];
+        let value =email.value;
+        if (value.match(emailPattern)){
+            console.log(true);
+            displaymsg.textContent = 'Thank You! Your email address ' + email + ' has been added to our mailing list!';
+            
         }
-        else if (email != ""){
-            displaymsg.innerHTML = 'Thank You! Your email address ' + email + ' has been added to our mailing list!';
+        else {
+            console.log(false);
+            displaymsg.textContent = 'Please enter a valid email address.';
         }
     })
 
+
+  
+      
+      
 
 
 
