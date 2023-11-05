541a3b66119ecc7ca92f38b8ae694e396c2c5e87
Damelia Coleman <dameliac@outlook.com>
2023-11-05 10:15:59 -0500

    updates

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+window.onload = function newsletter(){
+
+    let email = document.getElementById("email").value;
+    let submit = document.getElementsByTagName("button")[0];
+    let displaymsg = document.getElementsByClassName("message");
+
+
+
+    submit.addEventListener('click', function(){
+        console.log(email);
+        if (email == ""){
+            displaymsg.innerHTML = 'Please enter a valid email address.';
+        }
+        else if (email != ""){
+            displaymsg.innerHTML = 'Thank You! Your email address ' + email + ' has been added to our mailing list!';
+        }
+    })
+
+
+
+
+}
